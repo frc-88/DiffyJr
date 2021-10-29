de97c654bac1401a2eb6cff0cc24763289ea5bc4
Woz4tetra <woz4tetra@gmail.com>
2021-10-28 23:29:37 -0400

    putting update back

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,7 +52,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         // Happens after mode periodic method
-        // this.swerve.update();
+        this.swerve.update();
         // m_swerve_table.update();
     }
 
